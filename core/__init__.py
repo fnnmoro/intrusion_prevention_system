2a87ffb60ec8cdb72d04675e4e41fff5dd1bd6d2
Fernando Luiz Moro <fernandoluizmoro@gmail.com>
2018-10-11 17:18:11 -0300

    Remove the application factory
    This change was made to improve simplicity in development. In the
    future, the need for this function will be evaluated.

--- core/__init__.py
+++ core/__init__.py
@@ -1,33 +1,31 @@
 import os
 import sqlite3
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flows.sqlite'),
-    )
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flows.sqlite'),
+    DEBUG=True
+)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+app.config.from_pyfile('config.py', silent=True)
 
-    from core import db
-    from core import train, dep
-    db.init_db(app)
+socketio = SocketIO(app)
 
-    @app.route('/')
-    @app.route('/home')
-    def home():
-        return render_template('home.html')
+from core import db
+from core import train, dep
 
-    app.register_blueprint(train.bp)
-    app.register_blueprint(dep.bp)
+db.init_db(app)
 
-    return app
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+app.register_blueprint(train.bp)
+app.register_blueprint(dep.bp)
+
+
+
