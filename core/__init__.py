f100a0fa0f5cb458b77b3704f989e22c71f11c6e
Fernando Luiz Moro <fernandoluizmoro@gmail.com>
2018-10-19 16:06:35 -0300

    Add the /about route

--- core/__init__.py
+++ core/__init__.py
@@ -5,7 +5,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
-    SECRET_KEY='dev',
+    SECRET_KEY='h7cn#403mks-',
     DATABASE=os.path.join(app.instance_path, 'flows.sqlite'),
     DEBUG=True
 )
@@ -14,15 +14,19 @@ app.config.from_pyfile('config.py', silent=True)
 
 socketio = SocketIO(app)
 
-from core import db
+from core import database
 from core import train, dep
 
-db.init_db(app)
+database.init_db(app)
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 app.register_blueprint(train.bp)
 app.register_blueprint(dep.bp)
