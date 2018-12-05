2e80ff576eabe9554281addcfe121204cca1b491
flmoro <fernandoluizmoro@gmail.com>
2018-12-05 11:31:01 -0200

    Add get_anomalous_flows in database module
    This method returns the IPs to show the blacklist.
    
    Some features are cast to allow them to be stored in the database.

--- core/database.py
+++ core/database.py
@@ -1,42 +1,37 @@
-import os
 import sqlite3
-from flask import g, current_app
+from core import app
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+def create_database():
+    db = sqlite3.connect(app.config['DATABASE'])
 
-    return g.db
+    with open('schema.sql') as sql_script:
+        db.executescript(sql_script.read())
 
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-
-def init_db(app):
-    with app.app_context():
-        db = get_db()
-        with current_app.open_resource('schema.sql') as f:
-            db.executescript(f.read().decode('utf8'))
+    db.close()
 
 
 def store_flows(flows):
-    db = sqlite3.connect('../instance/flows.sqlite')
+    db = sqlite3.connect(app.config['DATABASE'])
 
     for entry in flows:
         entry[7] = str(entry[7])
+        entry[-1] = int(entry[-1])
+
         db.execute(
             'INSERT INTO flows (ts, te, ismc, odmc, sa, da, pr, iflg, sp, dp, '
             'td, ipkt, ibyt, bps, bpp, pps, flw, lbl) VALUES (?, ?, ?, ?, ?, ?, '
             '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', entry)
-
-
     db.commit()
 
-    print("number of flows", db.execute('SELECT count(1) FROM flows').fetchall())
-    print("number of flows", db.execute('SELECT count(1) FROM flows').fetchone())
+    db.close()
+
+
+def get_anomalous_flows():
+    db = sqlite3.connect(app.config['DATABASE'])
+
+    anomalous_flows = db.execute('SELECT sa, da, COUNT(flw) FROM flows '
+                                 'WHERE lbl = 1 GROUP BY sa').fetchall()
+
+    db.close()
+
+    return anomalous_flows
