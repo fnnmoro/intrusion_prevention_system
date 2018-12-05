95f5d1f9058c70b03a39afdea93c671b72f01498
flmoro <fernandoluizmoro@gmail.com>
2018-12-05 11:27:02 -0200

    Add directory paths
    These paths are used across multiple modules.
    
    A small refactoring process was done.

--- core/__init__.py
+++ core/__init__.py
@@ -1,23 +1,31 @@
 import os
-import sqlite3
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
+from model import tools
+
+
+dataset_path = "/home/flmoro/bsi16/research_project/codes/dataset/"
+pcap_path = "/home/flmoro/bsi16/research_project/codes/dataset/pcap/"
+nfcapd_path = "/home/flmoro/bsi16/research_project/codes/dataset/nfcapd/"
+csv_path = "/home/flmoro/bsi16/research_project/codes/dataset/csv/"
+log_path = "/home/flmoro/bsi16/research_project/codes/log/"
+
+tools.check_path_exist(pcap_path, nfcapd_path, csv_path, log_path)
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    SECRET_KEY='h7cn#403mks-',
-    DATABASE=os.path.join(app.instance_path, 'flows.sqlite'),
-    DEBUG=True
-)
+app.config.from_mapping(SECRET_KEY='h7cn#403mks-',
+                        DATABASE=os.path.join(app.instance_path,
+                                              'network_data.db'),
+                        DEBUG=True)
 
 app.config.from_pyfile('config.py', silent=True)
 
 socketio = SocketIO(app)
 
-from core import database
+import database
 from core import train, dep
 
-database.init_db(app)
+database.create_database()
 
 @app.route('/')
 @app.route('/home')
--- core/run.py
+++ core/run.py
@@ -1,5 +1,6 @@
 from core import app
 from core import socketio
 
+
 if __name__ == '__main__':
     socketio.run(app)
