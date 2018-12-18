62191c112d701c5c38d03572c1e1fe0512cbfb87
flmoro <fernandoluizmoro@gmail.com>
2018-12-18 11:56:40 -0200

    Add database methods to the realtime module
    The new database methods created are called in this module to allow
    storing the anomalous flows and generate the blacklist in real time.

--- core/realtime.py
+++ core/realtime.py
@@ -1,28 +1,26 @@
 import time
-from model import gatherer
-from model import tools
-from view import export_flows
+from model import gatherer, tools
 from model.preprocessing import Formatter, Modifier
+from model.mitigation import Mitigator
 from threading import Thread
 from core import socketio
-from database import store_flows
+import database
 from core import nfcapd_path, csv_path
+import pickle
+
 
 class WorkerThread(Thread):
-    def __init__(self, dt, ex, event, model,
-                 choice_features, preprocessing, dataset_type):
+    def __init__(self, event, model, choice_features, dataset_type):
         super().__init__()
         self.thread_stop_event = event
-        self.dt = dt
-        self.ex = ex
         self.model = model
         self.choice_features = choice_features
-        self.preprocessing = preprocessing
         self.dataset_type = dataset_type
+        self.dt = pickle.load(open('../objects/dt', 'rb'))
+        self.ex = pickle.load(open('../objects/ex', 'rb'))
 
     def model_execution(self):
         print('thread execution')
-        total_anomalies = 0
 
         process = gatherer.nfcapd_collector(nfcapd_path, 60)
 
@@ -43,14 +41,13 @@ class WorkerThread(Thread):
                     flows, file_name = gatherer.open_csv(path, files[0],
                                                          -1, True)
 
-                    tools.clean_files(nfcapd_path, csv_path, True)
+                    tools.clean_tmp_files(nfcapd_path, csv_path, True)
 
                     ft = Formatter(flows)
                     header, flows = ft.format_flows()
 
                     md = Modifier(flows, header)
                     header, flows = md.modify_flows(100, self.dataset_type)
-                    print(flows)
 
                     header_features, features = self.ex.extract_features(
                         header, flows, self.choice_features)
@@ -58,20 +55,22 @@ class WorkerThread(Thread):
                     pred, test_date, test_dur = self.dt.execute_classifiers(
                         features, 0)
 
-                    flows, anomalies = self.dt.find_anomalies(flows,
-                                                              pred)
+                    anomalous_flows = self.dt.find_anomalies(flows, pred)
 
-                    export_flows(flows,
-                                 csv_path + "flows/",
-                                 file_name.split(".csv")[0] + "_w60.csv",
-                                 header)
+                    if anomalous_flows:
+                        database.tmp_flows(anomalous_flows)
 
-                    store_flows(flows)
+                        blacklist = database.get_anomalous_flows()
 
-                    total_anomalies += anomalies
+                        mitigation = Mitigator(blacklist)
+                        mitigation.insert_rule()
 
-                    socketio.emit('mytest', {'total_anomalies': total_anomalies},
+                    socketio.emit('mytest',
+                                  {'total_anomalies':
+                                    database.get_num_anomalous_flows()},
                                   namespace='/test')
+
+                    database.delete_tmp_flows()
 
                 time.sleep(2)
         finally:
