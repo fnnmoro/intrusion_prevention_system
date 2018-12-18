d765c2b76974aef4faef7d4d1bb813b5a7a7940a
flmoro <fernandoluizmoro@gmail.com>
2018-12-18 11:49:36 -0200

    Add mitigate and false_positive route
    The mitigate route shows the blacklist.
    
    The false_positive route excludes possible false positive flows.

--- core/dep.py
+++ core/dep.py
@@ -1,8 +1,8 @@
-import pickle
 from threading import Thread, Event
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect, url_for
 import realtime
 import database
+from model.mitigation import Mitigator
 
 thread = Thread()
 thread_stop_event = Event()
@@ -12,30 +12,39 @@ bp = Blueprint('dep', __name__, url_prefix='/dep')
 @bp.route('/detect', methods=['GET', 'POST'])
 def detect():
     global thread
-
-    dt = pickle.load(open('../objects/dt', 'rb'))
-    ex = pickle.load(open('../objects/ex', 'rb'))
+    num_anomalous_flows = 0
 
     if not thread.isAlive():
-        thread = realtime.WorkerThread(dt, ex, thread_stop_event,
+        thread = realtime.WorkerThread(thread_stop_event,
                                        [int(request.form['model'])],
                                        session['choice_features'],
-                                       session['preprocessing'],
                                        session['dataset_type'])
         thread.start()
+    else:
+        num_anomalous_flows = database.get_num_anomalous_flows()
 
-    return render_template('dep/detect.html')
+    return render_template('dep/detect.html',
+                           num_anomalous_flows=num_anomalous_flows)
 
-@bp.route('/blacklist')
-def blacklist():
-    text_info = ['target', 'anomalous flows']
+@bp.route('/mitigate')
+def mitigate():
+    blacklist = database.get_blacklist()
 
-    anomalous_flows = database.get_anomalous_flows()
+    if not blacklist:
+        return redirect(url_for('dep.detect'))
 
-    return render_template('dep/blacklist.html',
+    text_info = ['attacker', 'target', 'anomalous flows', 'time']
+
+    return render_template('dep/mitigate.html',
                            text_info=text_info,
-                           anomalous_flows=anomalous_flows)
+                           blacklist=blacklist)
 
-@bp.route('/mitigation', methods=['GET', 'POST'])
-def mitigation():
-    return render_template('dep/mitigation.html')
+@bp.route('/false_positive', methods=['GET', 'POST'])
+def false_positive():
+    if request.method == 'POST':
+        database.delete_false_positive(request.form['false_positive'])
+
+        mitigation = Mitigator()
+        mitigation.delete_rule(request.form['false_positive'])
+
+    return redirect(url_for('dep.mitigate'))
