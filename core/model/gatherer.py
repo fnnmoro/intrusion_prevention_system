06810f8ff85321059e392f3971eb3d3a2cd200f4
flmoro <fernandoluizmoro@gmail.com>
2018-12-20 10:45:18 -0200

    Refactor split_pcap method
    The main changes was due to refactoring done in the directory_content
    method.

--- core/model/gatherer.py
+++ core/model/gatherer.py
@@ -1,46 +1,46 @@
 import os
 import csv
 import subprocess
-from .tools import processing_time_log
+from .tools import make_dir, processing_time_log
 
 
-def split_pcap(pcap_path, pcap_files, size=0):
+def split_pcap(pcap_path, pcap_files, split_size):
+    """Splits pcap files according to the chosen size.
+
+    Parameters
+    ----------
+    pcap_path: str
+        Absolute pcap path.
+    pcap_files: list
+        All pcap files to be split.
+    split_size: int
+        Size of the split.
+
+    Raises
+    ----------
+    subprocess.CalledProcessError
+        Entered a invalid value less or equal to zero."""
+
+    print(end=f'{"-" * 10}\n')
+
     try:
-        # avoids to split if there isn't necessary
-        if size != 0:
+        make_dir(f'{pcap_path}/split/')
 
-            # creates the split temporary folder
-            if not os.path.exists(pcap_path + "split/"):
-                os.system("mkdir {0}split/".format(pcap_path))
+        # splits multiple pcap files
+        for pcap_file in pcap_files:
+            # executes tcpdump program to split the pcap files
+            subprocess.run(f'tcpdump -r {pcap_path}/{pcap_file} -w {pcap_path}/'
+                           f'split/{pcap_file.split(".pcap")[0]} -C {split_size}',
+                           shell=True, check=True)
 
-            print()
-            start_idx = int(input("choose the initial pcap file: "))-1
-            final_idx = int(input("choose the final pcap file: "))-1
-            print()
+            print(pcap_path + pcap_file, end=f'\n{"-" * 10}\n')
 
-            # loop to split multiple pcap files
-            for i in range(start_idx, final_idx+1):
-                file_name = pcap_files[i].split(".pcap")[0]
-
-                # executes tcpdump program to split the pcap files
-                subprocess.run("tcpdump -r {0}{1} -w {0}split/{2} -C {3}"
-                               .format(pcap_path, pcap_files[i], file_name, size), shell=True,
-                               check=True)
-
-                print(pcap_path + pcap_files[i], end="\n\n")
-
-            # renames all split pcap files
-            for file in sorted(os.listdir(pcap_path + "split/")):
-                os.rename(pcap_path + "split/" + file, "{0}{1}.pcap".format(pcap_path + "split/", file))
-    except subprocess.CalledProcessError as error:
-        print()
-        print(error, end="\n\n")
-    except ValueError as error:
-        print()
-        print(error, end="\n\n")
-    except IndexError as error:
-        print()
-        print(error, end="\n\n")
+        # renames all split pcap files
+        for file in sorted(os.listdir(f'{pcap_path}/split/')):
+            os.rename(f'{pcap_path}/split/{file}',
+                      f'{pcap_path}/split/{file}.pcap')
+    except subprocess.CalledProcessError:
+        print('size must be greater than 0', end=f'\n{"-" * 10}\n')
 
 
 def convert_pcap_nfcapd(pcap_path, pcap_files, nfcapd_path, win_time=60):
