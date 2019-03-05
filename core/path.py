fe4abedbf134bf5dabb913114a85974ce4211898
flmoro <fernandoluizmoro@gmail.com>
2019-03-05 13:05:30 -0300

    Remove obj path

--- core/path.py
+++ core/path.py
@@ -4,6 +4,5 @@ base_path = '/home/flmoro/bsi16/research_project/anomaly_detection/' \
 paths = {'pcap': f'{base_path}' + 'pcap/',
          'nfcapd': f'{base_path}' + 'nfcapd/',
          'csv': f'{base_path}' + 'csv/',
-         'obj': f'{base_path}' + 'obj/',
          'saves': f'{base_path}' + 'saves/',
          'log': f'{base_path}' + 'log/'}
