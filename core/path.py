8f23efa0aa5e5b15ab36a17bbe3d9075395187e6
flmoro <fernandoluizmoro@gmail.com>
2019-03-05 09:45:16 -0300

    Add the path module
    This module contains all the major paths to the folders used by the 
    application.

---
+++ core/path.py
@@ -0,0 +1,9 @@
+base_path = '/home/flmoro/bsi16/research_project/anomaly_detection/' \
+            'codes/triple_m_ads/'
+
+paths = {'pcap': f'{base_path}' + 'pcap/',
+         'nfcapd': f'{base_path}' + 'nfcapd/',
+         'csv': f'{base_path}' + 'csv/',
+         'obj': f'{base_path}' + 'obj/',
+         'saves': f'{base_path}' + 'saves/',
+         'log': f'{base_path}' + 'log/'}
