7fc511afc0035d68695c650ff90f6c8bf13484a4
flmoro <fernandoluizmoro@gmail.com>
2019-05-04 16:02:06 -0300

    Refactor folders dict in run module

--- core/run.py
+++ core/run.py
@@ -2,11 +2,11 @@ from core import app
 from core import socketio
 from core.model.database import delete_blacklist
 from core.model.tools import clean_files
-from path import paths
+from path import folders
 
 if __name__ == '__main__':
     try:
         socketio.run(app)
     finally:
         delete_blacklist()
-        clean_files([paths['nfcapd']], ['*'])
+        clean_files(folders['nfcapd'], '*')
