5fd2f1332654ba0a98fbc9e4c9aad439e5900c8c
flmoro <fernandoluizmoro@gmail.com>
2019-05-04 15:17:55 -0300

    Update docstrings in the gatherer module
    Added dot at the end of each sentence.

--- tests/test_gatherer.py
+++ tests/test_gatherer.py
@@ -25,11 +25,11 @@ pcap_file = get_files(pcap_path)
 
 # unit tests
 class TestSplitPcap(unittest.TestCase):
-    """Tests the split_pcap function in gatherer module"""
+    """Tests the split_pcap function in gatherer module."""
 
     @classmethod
     def setUpClass(cls):
-        """Initiates the parameters to feed the test function"""
+        """Initiates the parameters to feed the test functions."""
 
         cls.split_size = 300
 
@@ -38,19 +38,19 @@ class TestSplitPcap(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """Cleans all files used in tests"""
+        """Cleans all files used in tests."""
 
         clean_files([f'{pcap_path}split/'], ['*'])
         os.system(f'rm -rf {pcap_path}split')
 
     def test_num_files(self):
-        """Tests the number of files created by splitting the pcap files"""
+        """Tests the number of files created by splitting the pcap files."""
 
         self.assertEqual(len(get_files(f'{pcap_path}split/')), 4,
                          'different number of pcap files')
 
     def test_last_file_size(self):
-        """Tests if the last file generated has the expected remaining size"""
+        """Tests if the last file generated has the expected remaining size."""
 
         pcap_files = get_files(f'{pcap_path}split/')
 
@@ -63,31 +63,31 @@ class TestSplitPcap(unittest.TestCase):
 
 
 class TestConvertPcapNfcapd(unittest.TestCase):
-    """Tests the convert_pcap_nfcapd function in gatherer module"""
+    """Tests the convert_pcap_nfcapd function in gatherer module."""
 
     @classmethod
     def setUpClass(cls):
-        """Initiates the parameters to feed the test function"""
+        """Initiates the parameters to feed the test functions."""
 
         # function to be tested
         convert_pcap_nfcapd(pcap_path, pcap_file, nfcapd_path, 60)
 
     @classmethod
     def tearDownClass(cls):
-        """Cleans all files used in tests"""
+        """Cleans all files used in tests."""
 
         clean_files([f'{nfcapd_path}'], ['*'])
 
     def test_num_files(self):
         """Tests if the number of files match if the number of minutes in the
-        pcap file"""
+        pcap file."""
 
         self.assertEqual(len(get_files(nfcapd_path)), 14,
                          'different number of nfcapd files')
 
     def test_time_interval(self):
         """Tests if the time interval between the nfcapd files is according to
-        the defined time"""
+        the defined time."""
 
         nfcapd_files = get_files(nfcapd_path)
 
@@ -100,11 +100,11 @@ class TestConvertPcapNfcapd(unittest.TestCase):
 
 
 class TestConvertNfcapdCSV(unittest.TestCase):
-    """Tests the convert_nfcapd_csv function in gatherer module"""
+    """Tests the convert_nfcapd_csv function in gatherer module."""
 
     def setUp(self):
-        """Initiates the parameters to feed the test function and previous
-        function to generated the necessary files"""
+        """Initiates the parameters to feed the test functions and previous
+        functions to generated the necessary files."""
 
         convert_pcap_nfcapd(pcap_path, pcap_file, nfcapd_path, 60)
 
@@ -113,14 +113,14 @@ class TestConvertNfcapdCSV(unittest.TestCase):
         convert_nfcapd_csv(nfcapd_path, nfcapd_files, csv_path, 'test')
 
     def tearDown(self):
-        """Cleans all files used in tests"""
+        """Cleans all files used in tests."""
 
         clean_files([f'{nfcapd_path}'], ['*'])
         clean_files([f'{csv_path}'], ['*'])
 
     def test_file_interval(self):
         """Tests if the csv file interval generate by the nfcapd files is
-        correct"""
+        correct."""
 
         csv_file = get_files(csv_path)[0]
 
@@ -141,12 +141,12 @@ class TestConvertNfcapdCSV(unittest.TestCase):
 
 
 class TestOpenCSV(unittest.TestCase):
-    """Tests the open_csv function in gatherer module"""
+    """Tests the open_csv function in gatherer module."""
 
     @classmethod
     def setUpClass(cls):
-        """Initiates the parameters to feed the test function and previous
-        functions to generated the necessary files"""
+        """Initiates the parameters to feed the test functions and previous
+        functions to generated the necessary files."""
 
         convert_pcap_nfcapd(pcap_path, pcap_file, nfcapd_path, 60)
 
@@ -160,28 +160,28 @@ class TestOpenCSV(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """Cleans all files used in tests"""
+        """Cleans all files used in tests."""
 
         clean_files([f'{nfcapd_path}'], ['*'])
         clean_files([f'{csv_path}'], ['*'])
 
     def test_header_length(self):
-        """Tests the length of the header based on the number of features"""
+        """Tests the length of the header based on the number of features."""
 
         self.assertEqual(len(self.header), 48)
 
     def test_flow_length(self):
-        """Tests the length of the flow based on the number of features"""
+        """Tests the length of the flow based on the number of features."""
 
         self.assertEqual(len(self.flows[0]), 48)
 
     def test_num_flows(self):
-        """Tests the number of flows to verify the sample process"""
+        """Tests the number of flows to verify the sample process."""
 
         self.assertEqual(len(self.flows), 30)
 
     def test_last_flow(self):
-        """Tests the values of the features from the last flow"""
+        """Tests the values of the features from the last flow."""
 
         last_flow = ['2015-11-24 13:40:52', '2015-11-24 13:40:52', '0.000',
                      '146.164.69.203', '224.0.0.251', '0', '0', 'IGMP',
@@ -198,23 +198,23 @@ class TestOpenCSV(unittest.TestCase):
 
 
 class TestCaptureNfcapd(unittest.TestCase):
-    """Tests the capture_nfcapd function in gatherer module"""
+    """Tests the capture_nfcapd function in gatherer module."""
 
     def setUp(self):
-        """Initiates the parameters to feed the test function"""
+        """Initiates the parameters to feed the test function."""
 
         self.process = capture_nfcapd(nfcapd_path, 60)
 
     def tearDown(self):
         """Cleans all files used in tests and kills the process started by the
-        function"""
+        function."""
 
         clean_files([f'{nfcapd_path}'], ['*'])
         self.process.kill()
 
     def test_file_creation(self):
         """Tests if the process started by the function is creating the files
-        correctly"""
+        correctly."""
 
         # delay one second to allow the start of the process
         time.sleep(1)
