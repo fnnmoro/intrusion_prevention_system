81e10d521db743e0f8b870a8e713c1a0ad59746e
flmoro <fernandoluizmoro@gmail.com>
2019-06-05 19:56:50 -0300

    Refactor hiddenForm method
    This function was used only to hide the forms and to display a message 
    when the machine learning algorithms were training. The message name was 
    modified to be more generic to use in other parts of the application.
    
    Some unnecessary things were removed from the method.

--- core/static/content.js
+++ core/static/content.js
@@ -12,12 +12,9 @@ $(document).ready(function changeColor() {
 
 function hiddenForm() {
     var p = document.createElement('p')
-    var text = document.createTextNode('Training...');
+    var text = document.createTextNode('Loading...');
 
     p.appendChild(text);
-    p.setAttribute('id', 'msgTraining');
-
     document.querySelector('main').appendChild(p);
-
-    document.getElementById('config').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
 };
