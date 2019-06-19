e2a7135cd7680b26a958b08033f0c1b010089240
flmoro <fernandoluizmoro@gmail.com>
2019-06-19 16:12:10 -0300

    Remove clean_files method

--- core/run.py
+++ core/run.py
@@ -2,11 +2,10 @@ from core import app
 from core import socketio
 from core.model.database import delete_blacklist
 from core.model.tools import clean_files
-from path import folders
+from path import paths
 
 if __name__ == '__main__':
     try:
         socketio.run(app)
     finally:
         delete_blacklist()
-        clean_files(folders['nfcapd'], '*')
