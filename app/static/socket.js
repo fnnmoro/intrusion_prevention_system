692fb11696287a956aa20e40a95f6fc5d769fc83
flmoro <fernandoluizmoro@gmail.com>
2019-07-20 12:16:01 -0300

    Update urls according to new routes names

--- app/static/socket.js
+++ app/static/socket.js
@@ -1,9 +1,9 @@
 $(document).ready(function changeColor() {
     // connect to the server
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/dep');
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/detection');
 
     // receive details from server
-    socket.on('detect', function(msg) {
+    socket.on('realtime', function(msg) {
         if (msg['anomalous_flows'] > 0){
             $('#anomalies').html(msg['anomalous_flows']).css('color', 'red');
         }
