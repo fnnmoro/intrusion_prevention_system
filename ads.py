0e078b46702b2503693f50f4dcbec17b0d74408a
flmoro <fernandoluizmoro@gmail.com>
2019-07-20 12:10:14 -0300

    Add clean_fileds method in ads file

--- ads.py
+++ ads.py
@@ -1,9 +1,12 @@
 from app import app, socketio
 from app import database as db
+from app.core import tools
+from app.path import paths
 
 
 if __name__ == '__main__':
     try:
         socketio.run(app)
-    finally:        
+    finally:
         db.delete_blacklist()
+        tools.clean_files(paths['nfcapd'], '*')
