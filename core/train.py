0573cc22d0e8253a1720c48be56758a628d1b1ae
flmoro <fernandoluizmoro@gmail.com>
2019-07-20 08:26:32 -0300

    Refactor train views
    The datasets view have was to retrieve the contents of the dataset 
    folder.
    
    The outcome of the results view was changed to a new, more organized and 
    easy-to-retrieve dictionary.

--- core/train.py
+++ core/train.py
@@ -8,34 +8,38 @@ from path import paths
 from model.detection import Detector
 from model.gatherer import open_csv
 from model.preprocess import Extractor, Formatter, Preprocessor
-from model.tools import evaluation_metrics
-from model.walker import get_files
+from model.tools import evaluation_metrics, get_content
 
 
-ex = Extractor(8, list())
-dt = Detector()
-flows = None
+flows = list()
 
 bp = Blueprint('train', __name__, url_prefix='/train')
 
+
 @bp.route('/load')
 def load():
-    files = get_files(f'{paths["saves"]}')
-
-    return render_template('train/load.html', files=files)
+    return render_template('train/load.html',
+                           files=get_content(f'{paths["saves"]}')[1])
 
 
 @bp.route('/dataset')
 def datasets():
+    datasets_names = list()
+    datasets = get_content(f'{paths["csv"]}datasets/')[1]
+    for dataset in datasets:
+        tmp = dataset.split('.csv')[0].capitalize()
+        datasets_names.append(' '.join(tmp.split('_')))
+
     return render_template('train/datasets.html',
-                           datasets=get_files(f'{paths["csv"]}datasets/'),
-                           preprocesses=Preprocessor.methods.keys())
+                           datasets=datasets,
+                           datasets_names=datasets_names,
+                           preprocess_methods=Preprocessor.methods)
+
 
 @bp.route('/classifiers', methods=['GET', 'POST'])
 def classifiers():
+    global flows
     if request.method == 'POST':
-        global flows
-
         header, flows = open_csv(f'{paths["csv"]}datasets/',
                                  request.form['dataset'],
                                  int(request.form['sample']))
@@ -43,66 +47,80 @@ def classifiers():
         for entry in flows:
             Formatter.convert_features(entry, True)
 
-        change_features_name = False
+        extractor = Extractor(8, list())
         if 'flw' in header:
-            change_features_name = True
-            setattr(ex, 'features_idx', 6)
+            setattr(extractor, 'start_index', 6)
+        features_names = extractor.extract_features_names()
 
-        session['test_set_size'] = int(request.form['test_set_size'])/100
+        session['start_index'] = getattr(extractor, 'start_index')
+        session['test_size'] = int(request.form['test_size'])/100
         session['k-folds'] = int(request.form['k-folds'])
-        session['preprocess'] = request.form['preprocess']
+        session['preprocess_key'] = request.form['preprocess_key']
 
         return render_template('train/classifiers.html',
-                               classifiers=list(getattr(dt,
-                                                        'classifiers').keys()),
-                               change_features_name=change_features_name)
+                               classifiers=getattr(Detector(), 'classifiers'),
+                               features_names=features_names)
 
 
 @bp.route('/results', methods=['GET', 'POST'])
 def results():
     if request.method == 'POST':
         if request.referrer.split('/')[-1] == 'classifiers':
-            setattr(ex, 'selected_features', [int(idx) for idx in
-                                              request.form
-                                              .getlist('features_name')])
+            extractor = Extractor(session['start_index'],
+                                  [int(idx) for idx in
+                                  request.form.getlist('features_name')])
 
-            features, labels = ex.extract_features(flows)
+            features, labels = extractor.extract_features(flows)
 
             dataset = train_test_split(features, labels,
-                                       test_size=session['test_set_size'],
+                                       test_size=session['test_size'],
                                        random_state=13,
                                        stratify=labels)
 
-            info = list()
-            tmp_dir = mkdtemp()
-            for clf in request.form.getlist('classifiers'):
-                dt.tuning_hyperparameters(clf,
-                                          Preprocessor.methods
-                                          [session['preprocess']],
-                                          session['k-folds'],
-                                          tmp_dir)
+            preprocess = Preprocessor.methods[session['preprocess_key']]['obj']
 
-                param, train_date, train_dur = dt.train_classifier(clf,
+            results = list()
+            # creates an absolute path of a temporary directory
+            tmp_dir = mkdtemp()
+            detector = Detector()
+            for clf in request.form.getlist('classifiers_keys'):
+                detector.tuning_hyperparameters(clf,
+                                                preprocess,
+                                                session['k-folds'],
+                                                tmp_dir)
+
+                param, train_date, train_dur = detector.train_classifier(clf,
                                                                    dataset[0],
                                                                    dataset[2])
 
-                pred, test_date, test_dur = dt.execute_classifier(clf,
+                pred, exec_date, exec_dur = detector.execute_classifier(clf,
                                                                   dataset[1])
 
-                results = ([clf, train_date, test_date, train_dur, test_dur]
-                            + evaluation_metrics(dataset[3], pred)
-                            + [session['preprocess'], param])
-                info.append(results)
-
+                outcome = {'clf':
+                              {'name': 'Classifier',
+                               'result': [clf, getattr(detector,
+                                               'classifiers')[clf]['name']]}}
+                outcome.update(evaluation_metrics(dataset[3], pred))
+                outcome.update({'tdt': {'name': 'Training date',
+                                        'result': train_date},
+                                'edt': {'name': 'Execution date',
+                                        'result': exec_date},
+                                'tdur': {'name': 'Training duration',
+                                         'result': train_dur},
+                                'edur': {'name': 'Execution duration',
+                                         'result': exec_dur},
+                                'param': {'name': 'Hyperparameters',
+                                          'result': param}})
+                results.append(outcome)
             rmtree(tmp_dir)
-            session['obj_date'] = datetime.now().strftime("%Y%m%d_%H%M%S")
-            pickle.dump([ex, dt, info],
-                        open(f'saves/obj_{session["obj_date"]}', 'wb'))
+            session['obj_name'] = f'obj_{datetime.now().strftime("%Y%m%d%H%M%S")}'
+            pickle.dump([extractor, detector, results],
+                        open(f'{paths["saves"]}{session["obj_name"]}', 'wb'))
         else:
-            obj = pickle.load(open(f'saves/{request.form["file"]}', 'rb'))
-            pickle.dump(obj[0], open('obj/ex', 'wb'))
-            pickle.dump(obj[1], open('obj/dt', 'wb'))
-            info = obj[2]
+            session['obj_name'] = f'{request.form["file"]}'
+            obj = pickle.load(open(f'{paths["saves"]}{session["obj_name"]}',
+                                   'rb'))
+            results=obj[2]
 
         return render_template('train/results.html',
-                               info=info)
+                               results=results)
