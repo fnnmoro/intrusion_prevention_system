765869ee0312327ca06d5afa51b4aaded21a3c18
flmoro <fernandoluizmoro@gmail.com>
2019-07-20 11:01:18 -0300

    Update ads file

--- ads.py
+++ ads.py
@@ -1,9 +1,9 @@
 from app import app, socketio
-from app.core.database import delete_blacklist
+from app import database as db
 
 
 if __name__ == '__main__':
     try:
         socketio.run(app)
-    finally:
-        delete_blacklist()
+    finally:        
+        db.delete_blacklist()
