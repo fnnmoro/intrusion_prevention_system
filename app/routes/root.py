ba4e0495cd3f76f9270a9660ed80e2cccd143d30
flmoro <fernandoluizmoro@gmail.com>
2019-07-20 09:04:45 -0300

    Add routes folder and root view

---
+++ app/routes/root.py
@@ -0,0 +1,12 @@
+from app import app
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
