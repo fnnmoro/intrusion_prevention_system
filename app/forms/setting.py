41a04cbe38bb958f9dabde319977d47304f91ec7
flmoro <fernandoluizmoro@gmail.com>
2019-08-04 14:17:38 -0300

    Add DatasetForm class

--- app/forms/setting.py
+++ app/forms/setting.py
@@ -1,6 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, SubmitField
+from wtforms import IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.widgets.html5 import NumberInput
 
 
 class LoadForm(FlaskForm):
     model = SubmitField('Model')
+
+
+class DatasetForm(FlaskForm):
+    dataset = SelectField('Datasets')
+    preprocessing =  SelectField('Preprocessing methods')
+    sample = IntegerField('Sample size',
+                          widget=NumberInput(min=-1),
+                          validators=[DataRequired])
+    division = IntegerField('Test set size',
+                            widget=NumberInput(min=5, max=95),
+                            validators=[DataRequired])
+    kfolds = IntegerField('Cross-validation folds',
+                          widget=NumberInput(min=1, max=20),
+                          validators=[DataRequired])
+    submit = SubmitField('Submit')
