d674c3942459da47eaffd734c4371d80d4c0ad8f
flmoro <fernandoluizmoro@gmail.com>
2019-08-05 21:11:56 -0300

    Add SQLAlchemy settins in config file

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    DATABASE = os.path.join(basedir, 'instance/network_data.db')
     DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    DATABASE = os.path.join(base_dir, 'instance/network_data.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'instance/app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
