f822b03966c4cfb4d79d38d0b9ca76a5d997dc97
flmoro <fernandoluizmoro@gmail.com>
2019-08-08 21:03:54 -0300

    Add ClassifierForm class

--- app/forms/setting.py
+++ app/forms/setting.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SelectField, SubmitField
+from wtforms import IntegerField, SelectField, SelectMultipleField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
+from wtforms.widgets import CheckboxInput, ListWidget, Input
 from wtforms.widgets.html5 import NumberInput
 
 
@@ -10,14 +11,26 @@ class LoadForm(FlaskForm):
 
 class DatasetForm(FlaskForm):
     dataset = SelectField('Datasets')
-    preprocessing =  SelectField('Preprocessing methods')
     sample = IntegerField('Sample size',
                           widget=NumberInput(min=-1),
                           validators=[DataRequired(), NumberRange(-1)])
-    division = IntegerField('Test set size',
+    split = IntegerField('Split size',
                             widget=NumberInput(min=5, max=95),
                             validators=[DataRequired(), NumberRange(5, 95)])
     kfolds = IntegerField('Cross-validation folds',
                           widget=NumberInput(min=1, max=20),
                           validators=[DataRequired(), NumberRange(1, 20)])
+    submit = SubmitField('Submit')
+
+
+class ClassifierForm(FlaskForm):
+    classifiers = SelectMultipleField('Machine learning classifiers',
+                                      widget=ListWidget(prefix_label=False),
+                                      option_widget=CheckboxInput(),
+                                      validators=[DataRequired()])
+    preprocessing = SelectField('Preprocessing methods', coerce=int)
+    features = SelectMultipleField('IP flows fetures',
+                                   widget=ListWidget(prefix_label=False),
+                                   option_widget=CheckboxInput(),
+                                   validators=[DataRequired()])
     submit = SubmitField('Submit')
