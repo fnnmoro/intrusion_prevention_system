425afabff6001394e847851e36c22529effa50ac
flmoro <fernandoluizmoro@gmail.com>
2019-08-10 11:47:59 -0300

    Add shell context processor

--- ads.py
+++ ads.py
@@ -1,12 +1,24 @@
-from app import app, socketio
-from app import database as db
+from app import app, db, socketio
 from app.core import tools
 from app.paths import paths
+from app.models.classifier import Classifier
+from app.models.dataset import Dataset
+from app.models.feature import Feature
+from app.models.model import Model
+from app.models.preprocessing import Preprocessing
+from app.models.result import Result
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'app': app, 'db': db, 'socketio': socketio,
+            'Classifier': Classifier, 'Dataset': Dataset, 'Feature': Feature,
+            'Model': Model, 'Preprocessing': Preprocessing, 'Result': Result}
 
 
 if __name__ == '__main__':
     try:
         socketio.run(app)
     finally:
-        db.delete_blacklist()
+        #db.delete_blacklist()
         tools.clean_files(paths['nfcapd'], '*')
