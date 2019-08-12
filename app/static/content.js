6160c695856b9e41c8d492718867a714ab5a804a
flmoro <fernandoluizmoro@gmail.com>
2019-08-12 20:16:45 -0300

    Add msg parameter in hiddenForm function
    This msg is showing when training and retraining the models.

--- app/static/content.js
+++ app/static/content.js
@@ -1,6 +1,6 @@
-function hiddenForm() {
+function hiddenForm(msg) {
     var p = document.createElement('p')
-    var text = document.createTextNode('Loading...');
+    var text = document.createTextNode(msg);
 
     p.appendChild(text);
     document.querySelector('main').appendChild(p);
