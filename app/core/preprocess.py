348c244baaa610a4d8e8e046fe41bfb1d6375663
flmoro <fernandoluizmoro@gmail.com>
2019-08-25 16:26:53 -0300

    Refactor preprocess module
    The staticmethod decorator was removed from the Formatter class so that 
    only the format_header and format_flows methods are used. New boolean 
    attributes were added to signal when specific processes are occurring.
    
    All changes made to the Modifier class were to make it possible to 
    aggregate one flow at a time instead of all. This procedure is essential 
    in RealtimeThread when intrusions should be blocked. Waiting for all 
    flows to aggregate can take a long time, allowing the attack to 
    continue.
    
    The Extractor class was changed to extract one flow at a time similar to 
    the Modifier class procedure.

--- app/core/preprocess.py
+++ app/core/preprocess.py
@@ -1,7 +1,6 @@
 import ast
 from datetime import datetime
 
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import (StandardScaler, MinMaxScaler,
                                    MaxAbsScaler, RobustScaler,
                                    QuantileTransformer, Normalizer)
@@ -12,26 +11,24 @@ from app.core.tools import process_time_log
 class Formatter:
     """Formats the IP flows to be used by machine learning algorithms.
 
-    The formatted IP flows can be used in the Modifier class to aggregate or
-    created new features.
-
-    Parameters
-    ----------
-    header: list
-        Features description.
-    flows: list
-        IP flows.
+    The formatted IP flows can be used in the Modifier class.
 
     Attributes
     ----------
-    self.header: list
-        Formatted features description.
-    self.flows: list
-        Formatted IP flows."""
+    self.header: list of list
+        Features description.
+    self.flows: list of list
+        IP flows.
+    self.gather: bool
+        Signals the gathering process.
+    self.train: bool
+        Signals the training process."""
 
-    def __init__(self, header, flows):
+    def __init__(self, header, flows, gather=True, train=False):
         self.header = header
         self.flows = flows
+        self.gather = gather
+        self.train = train
 
     @process_time_log
     def format_header(self):
@@ -39,7 +36,7 @@ class Formatter:
 
         Returns
         -------
-        list
+        list of list
             Formatted header."""
 
         self.delete_features(self.header)
@@ -53,274 +50,231 @@ class Formatter:
 
         Returns
         -------
-        list
+        list of list
             Formatted IP flows."""
 
-        # deletes the summary lines
-        del self.flows[-3:]
+        if self.gather:
+            # deletes the summary lines
+            del self.flows[-3:]
 
-        for entry in self.flows:
-            self.delete_features(entry)
-            self.sort_features(entry)
-            self.replace_features(entry)
-            self.convert_features(entry)
-            self.count_flags(entry)
+        for flow in self.flows:
+            if not self.train:
+                self.delete_features(flow)
+                self.sort_features(flow)
+                self.replace_features(flow)
+                self.count_flags(flow)
+            self.convert_features(flow)
 
         return self.flows
 
-    @staticmethod
-    def delete_features(entry):
+    def delete_features(self, flow):
         """Deletes non-discriminative features.
 
         Parameters
         ----------
-        entry: list
-            IP flow entry."""
+        flow: list
+            IP flow."""
 
         # fwd and stos
-        del entry[9:11]
+        del flow[9:11]
         # from opkt to the end
-        del entry[11:]
+        del flow[11:]
 
-    @staticmethod
-    def sort_features(entry):
+    def sort_features(self, flow):
         """Sorts the features in a specific order.
 
         Parameters
         ----------
-        entry: list
-            IP flow entry."""
+        flow: list
+            IP flow."""
 
-        # updates existing entry with the new order
-        entry[:] = [entry[idx] for idx in [0, 1, 3, 4, 7, 8, 5, 6, 2, 9, 10]]
+        # updates existing flow with the new order
+        flow[:] = [flow[idx] for idx in [0, 1, 3, 4, 7, 8, 5, 6, 2, 9, 10]]
 
-    @staticmethod
-    def replace_features(entry):
+    def replace_features(self, flow):
         """Replaces the missing features.
 
         Parameters
         ----------
-        entry: list
-            IP flow entry."""
+        flow: list
+            IP flow."""
 
         default_values = {0: '0001-01-01 01:01:01', 1: '0001-01-01 01:01:01',
                           2: '000.000.000.000', 3: '000.000.000.000',
-                          4: 'nopr', 5: '......', 6: '0', 7: '0',
+                          4: 'NONE', 5: '......', 6: '0', 7: '0',
                           8: '0', 9: '0', 10: '0'}
 
-        for idx, feature in enumerate(entry):
+        for idx, feature in enumerate(flow):
             # checks missing features
             if not feature:
-                entry[idx] = default_values[idx]
+                flow[idx] = default_values[idx]
 
-    @staticmethod
-    def convert_features(entry, train=False):
+    def convert_features(self, flow):
         """Converts the features to specific data types.
 
         Parameters
         ----------
-        entry: list
-            IP flow entry.
-        train: bool
-            Value to flag if the program is in train mode."""
+        flow: list
+            IP flow."""
 
-        # ts, te
-        entry[0:2] = [datetime.strptime(i, '%Y-%m-%d %H:%M:%S')
-                      for i in entry[0:2]]
-        # pr
-        entry[4] = entry[4].lower()
-        # sp, dp
-        entry[6:8] = [int(i) for i in entry[6:8]]
-        # td
-        entry[8] = round(float(entry[8]))
-        # ibyt to the end
-        entry[9:] = [int(i) for i in entry[9:]]
+        flow[0:2] = [datetime.strptime(i, '%Y-%m-%d %H:%M:%S')
+                      for i in flow[0:2]]
+        flow[8] = round(float(flow[8]))
+        flow[9:] = [int(i) for i in flow[9:]]
 
         # checks if is in the train mode to convert properly
-        if train:
-            # flg
-            entry[5] = ast.literal_eval(entry[5])
-        else:
-            entry[5] = entry[5].lower()
+        if self.train:
+            flow[5:8] = [ast.literal_eval(feat) for feat in flow[5:8]]
 
-    @staticmethod
-    def count_flags(entry):
+    def count_flags(self, flow):
         """Counts TCP flags.
 
         Parameters
         ----------
-        entry: list
-            IP flow entry."""
+        flow: list
+            IP flow."""
 
         # checks if is the tcp protocol
-        if entry[4] == 'tcp':
+        if flow[4] == 'TCP':
             # flags order
-            flags = ['u', 'a', 's', 'f', 'r', 'p']
+            flags = ['U', 'A', 'S', 'F', 'R', 'P']
             # filters only the flags
-            entry[5] = [flag for flag in entry[5] if flag != '.']
+            flow[5] = [flag for flag in flow[5] if flag != '.']
             # creates a new list with the flags order and the count of them
-            entry[5] = [entry[5].count(flag) for flag in flags]
+            flow[5] = [flow[5].count(flag) for flag in flags]
         else:
-            entry[5] = [0]
+            flow[5] = [0]
 
 
 class Modifier:
     """Modifies the formatted IP flows to aggregate and create more
     discriminating features to be used by the machine learning algorithms.
 
-    Parameters
-    ----------
-    header: list
-        Formatted features description.
-    flows: list
-        Formatted IP flows.
-
     Attributes
     ----------
-    self.header: list
-        Modified features description.
-    self.flows: list
-        Modified IP flows."""
+    self.header: list of list
+        Formatted features description.
+    self.flows: list of list
+        Formatted IP flows.
+    label: int
+        Flow class.
+    self.threshold: int
+        Aggregation threshold to prevent that mulitple flows become just
+        one."""
 
-    def __init__(self, header, flows):
+    def __init__(self, header, flows, label, threshold):
         self.header = header
         self.flows = flows
+        self.label = label
+        self.threshold = threshold
 
-    @process_time_log
-    def aggregate_flows(self, threshold):
-        """Aggregates formatted IP flows according to a established threshold.
-
-        The flows are aggregated considering the same start hour and minute,
-        source address, destination address and protocol.
-
-        The threshold is useful when the flows to be aggregate are anomalous,
-        like a DoS attack.
-
-        Parameters
-        ----------
-        threshold: int
-            Aggregation threshold to prevent that mulitple entries become just
-            one.
+    def extend_header(self):
+        """Extendes header according to new aggregations features.
 
         Returns
         -------
-        self.header: list
-            Modified features description.
-        self.flows: list
-            Modified IP flows."""
+        self.header: list of list
+            Extended features description."""
 
-        # aggregated flows
-        aggregated_flows = list()
+        self.header.extend(['bps', 'bpp', 'pps', 'nsp', 'ndp', 'flw', 'lbl'])
 
-        while self.flows:
-            # separates the first flow that will be matched against the others
-            base_entry = self.flows.pop(0)
-
-            # keeps only the unique ports
-            sp = {base_entry[6]}
-            dp = {base_entry[7]}
-
-            # compares the value with the established threshold
-            # 1 is used instead of 0 because a flow is separated first
-            count = 1
-            # compares the first flows with the rest of the flows
-            for idx, entry in enumerate(self.flows):
-                # checks if the threshold was reached
-                if count < threshold:
-                    # default rules for aggregation
-                    rules = [base_entry[0].hour == entry[0].hour,
-                             base_entry[0].minute == entry[0].minute,
-                             base_entry[2:5] == entry[2:5]]
-
-                    if all(rules):
-                        self.aggregate_features(base_entry, entry)
-                        sp.add(entry[6])
-                        dp.add(entry[7])
-
-                        # marks the flow that was aggregated
-                        self.flows[idx] = [None]
-                        count += 1
-                else:
-                    break
-
-            # counts the quantity of unique ports
-            base_entry[6] = len(sp)
-            base_entry[7] = len(dp)
-            # time duration recalculed
-            base_entry[8] = base_entry[1].second - base_entry[0].second
-            # number of aggregated flows
-            base_entry.append(count)
-            aggregated_flows.append(base_entry)
-
-            # checks if aggregation happened
-            if count > 1:
-                # filters only the not aggregated flows
-                self.flows = [entry for entry in self.flows if entry != [None]]
-
-        self.flows = aggregated_flows
-        self.header.append('flw')
-
-        return self.header, self.flows
-
-    @staticmethod
-    def aggregate_features(base_entry, entry):
-        """Aggregates features of the current entry in the base entry.
-
-        Parameters
-        ----------
-        base_entry: list
-            Aggregated entry.
-        entry: list
-            Current entry to aggregate."""
-
-        # important conditional because some entries are out of order
-        if base_entry[1] < entry[1]:
-            # te
-            base_entry[1] = entry[1]
-        # flg
-        base_entry[5] = [x + y for x, y in zip(base_entry[5], entry[5])]
-        # td
-        base_entry[8] = base_entry[1] - base_entry[0]
-        # byt
-        base_entry[9] += entry[9]
-        # pkt
-        base_entry[10] += entry[10]
+        return self.header
 
     @process_time_log
-    def create_features(self, label):
-        """Creates new features based on bytes, packtes and flow duration.
+    def aggregate_flow(self):
+        """Aggregates a formatted IP flow according to a established threshold.
 
-        When aggregation is done first, the flows are aggregated to later
-        compute the new features.
+        The flow are aggregated considering the same start hour and minute,
+        source address, destination address and protocol.
+
+        The threshold is useful when the flow to be aggregate are anomalous,
+        like a DoS attack.
+
+        Returns
+        -------
+        self.flow: list
+            Modified IP flow."""
+
+        # separates the first flow that will be matched against the others
+        # use only the right features when intrusions flow
+        base = self.flows.pop(0)[:11]
+
+        # keeps only the unique ports
+        sp = {base[6]}
+        dp = {base[7]}
+
+        # compares the value with the established threshold
+        # 1 is used instead of 0 because a flow is separated first
+        count = 1
+        # compares the base flow with the rest of the flows
+        for idx, flow in enumerate(self.flows):
+            # checks if the threshold was reached
+            if count != self.threshold:
+                # default rules for aggregation
+                rules = [base[0].hour == flow[0].hour,
+                         base[0].minute == flow[0].minute,
+                         base[2:5] == flow[2:5]]
+
+                if all(rules):
+                    # avoiding entries out of order
+                    if base[1] < flow[1]:
+                        base[1] = flow[1]
+                    base[5] = [x+y for x, y in zip(base[5], flow[5])]
+                    base[8] = base[1] - base[0]
+                    base[9] += flow[9]
+                    base[10] += flow[10]
+                    sp.add(flow[6])
+                    dp.add(flow[7])
+                    # marks the flow that was aggregated
+                    self.flows[idx] = [None]
+                    count += 1
+            else:
+                break
+        base[6] = sp
+        base[7] = dp
+        # counts the quantity of unique ports
+        base.append(len(sp))
+        base.append(len(dp))
+        # time duration recalculed
+        base[8] = (base[1] - base[0]).seconds
+        # number of aggregated flows
+        base.append(count)
+
+        self.create_features(base)
+
+        # checks if aggregation happened
+        if count > 1:
+            # filters only the not aggregated flows
+            self.flows = [flow for flow in self.flows if flow != [None]]
+
+        return base
+
+    @process_time_log
+    def create_features(self, base):
+        """Creates new features based on bytes, packtes and flow duration.
 
         Parameters
         ----------
-        label: int
-            Class of each entry."""
+        base: list
+            IP flow base."""
 
-        for entry in self.flows:
-            # default value
-            bps, bpp, pps = [0, 0, 0]
+        # default value
+        bps, bpp, pps = [0, 0, 0]
 
-            # checks if the time duration isn't zero
-            if entry[8]:
-                # bytes per second
-                bps = int(round(entry[10] / entry[8]))
-                # packet per second
-                pps = int(round(entry[9] / entry[8]))
+        # checks if the time duration isn't zero
+        if base[8]:
+            # bytes per second
+            bps = int(round(base[10] / base[8]))
+            # packet per second
+            pps = int(round(base[9] / base[8]))
 
-            # checks if the packet value isn't zero
-            if entry[9]:
-                # bytes per packet
-                bpp = int(round(entry[10] / entry[9]))
+        # checks if the packet value isn't zero
+        if base[9]:
+            # bytes per packet
+            bpp = int(round(base[10] / base[9]))
 
-            entry[11:11] = [bps, bpp, pps]
-            entry.append(label)
-
-        self.header[11:11] = ['bps', 'bpp', 'pps']
-        self.header.append('lbl')
-
-        return self.header, self.flows
+        base[11:11] = [bps, bpp, pps]
+        base.append(self.label)
 
 
 class Extractor:
@@ -335,34 +289,31 @@ class Extractor:
         self.selected_features = selected_features
 
     @process_time_log
-    def extract_features(self, flows):
-        """Extracts features and labels from flows.
+    def extract_features(self, flow):
+        """Extracts features and label from flow.
 
         Parameters
         ----------
-        flows: list
-            Formatted and modified flows.
+        flow: list
+            Formatted and modified flow.
 
         Returns
         -------
         list
-            Features and labels to be split into appropriate sets."""
+            Features and label to be split into appropriate sets."""
 
-        features, labels = list(), list()
+        features, label = list(), list()
 
-        for entry in flows:
-            tmp = list()
-            for idx in self.selected_features:
-                tmp.append(entry[idx])
-            features.append(tmp)
-            labels.append(entry[-1])
+        for idx in self.selected_features:
+            features.append(flow[idx])
+        label.append(flow[-1])
 
-        return features, labels
+        return features, label
 
 
 # preprocessing methods to be used by detection instance.
 preprocessing_obj = {
-    'normal': None,
+    'none': None,
     'max_absolute_scaler': MaxAbsScaler(),
     'min_max_scaler': MinMaxScaler(),
     'robust_scaler': RobustScaler(),
