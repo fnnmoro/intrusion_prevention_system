9125831a42b1d3cb50fc2d686dada85169d74bf1
flmoro <fernandoluizmoro@gmail.com>
2019-08-27 20:34:24 -0300

    Update shell context in ads file

--- ads.py
+++ ads.py
@@ -1,20 +1,26 @@
 from app import app, db, socketio
 from app.core import tools
 from app.models import (Classifier, Dataset, Feature,
-                        Model, Preprocessing, Result)
+                        Intrusion, Model, Preprocessing,
+                        Result)
 from app.paths import paths
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'app': app, 'db': db, 'socketio': socketio,
-            'Classifier': Classifier, 'Dataset': Dataset, 'Feature': Feature,
-            'Model': Model, 'Preprocessing': Preprocessing, 'Result': Result}
+            'Classifier': Classifier, 'Dataset': Dataset,
+            'Feature': Feature, 'Intrusion': Intrusion,
+            'Model': Model, 'Preprocessing': Preprocessing,
+            'Result': Result}
 
 
 if __name__ == '__main__':
     try:
         socketio.run(app)
     finally:
-        #db.delete_blacklist()
+        for itr in Intrusion.query.all():
+            db.session.delete(itr)
+        db.session.commit()
+
         tools.clean_files(paths['nfcapd'], '*')
