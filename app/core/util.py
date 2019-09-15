6fc84b6fd71a6c98fa5ae857f8904c9d380532e4
flmoro <fernandoluizmoro@gmail.com>
2019-09-15 10:46:58 -0300

    Refactor tools module
    This module was renamed to util and some functions were placed in the 
    appropriate modules.

--- app/core/util.py
+++ app/core/util.py
@@ -1,18 +1,13 @@
-import csv
 import logging
 import os
 import time
 from datetime import datetime
 from pytz import timezone
 
-from sklearn.metrics import (accuracy_score, precision_score,
-                             recall_score, f1_score,
-                             confusion_matrix)
-
 from app.paths import paths
 
 
-logger = logging.getLogger('tools')
+logger = logging.getLogger('util')
 
 
 def make_directory(path, directory):
@@ -34,55 +29,7 @@ def make_directory(path, directory):
         count += 1
 
 
-def evaluation_metrics(test_labels, pred):
-    """Computes the evaluation metrics for a given prediction.
-
-    Parameters
-    ----------
-    test_labels: list
-        Test labels that represent the correct answer for the predictions.
-    pred: list
-        Prediction made by a machine learning model."""
-
-    tn, fp, fn, tp = confusion_matrix(test_labels, pred).ravel()
-
-    outcome = {
-        'accuracy': round(accuracy_score(test_labels, pred), 5),
-        'precision': round(precision_score(test_labels, pred), 5),
-        'recall': round(recall_score(test_labels, pred), 5),
-        'f1_score': round(f1_score(test_labels, pred), 5),
-        'tn': int(tn),
-        'fp': int(fp),
-        'fn': int(fn),
-        'tp': int(tp)
-    }
-
-    return outcome
-
-
-def export_flows_csv(header, flows, dst_path, file_name):
-    """Exports flows to CSV file.
-
-    Parameters
-    ----------
-    header: list
-        Features description.
-    flows: list
-        IP flows.
-    dst_path: str
-        Destination path where the file will be exported.
-    file_name: str
-        Name of CSV file."""
-
-    with open(f'{dst_path}{file_name}', mode='a') as file:
-        writer = csv.writer(file)
-
-        writer.writerow(header)
-        for entry in flows:
-            writer.writerow(entry)
-
-
-def process_time_log(func):
+def timing(func):
     """Decorator to record the time of a function.
 
     Parameters
@@ -121,7 +68,8 @@ def process_time_log(func):
         end = time.time()
         duration = round(end - start, 7)
 
-        logger.info(f'{func.__name__} duration: {duration}')
+        if func.__name__ != 'test': 
+            logger.info(f'{func.__name__} duration: {duration}')
 
         if func.__name__ == 'train' or func.__name__ == 'test':
             return result, date, duration
